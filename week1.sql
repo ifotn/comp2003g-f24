1a92b767116b3fb5e332a0dc9bd4d0cf370a4265
ifotn <Rich.Freeman@GeorgianCollege.ca>
2024-09-04 10:50:39 -0400

    lesson 1

---
+++ week1.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE comp2003g;
+
+USE comp2003g;
+
+CREATE TABLE restaurants (
+	name VARCHAR(50),
+    address VARCHAR(255),
+    priceRange INT,
+    logo BLOB,
+    email VARCHAR(50)
+);
+
+DROP TABLE restaurants;
+
+CREATE TABLE restaurants (
+	name VARCHAR(50),
+    address VARCHAR(255),
+    priceRange INT,
+    logo BLOB,
+    email VARCHAR(50),
+    phone CHAR(10)
+);
+
+INSERT INTO restaurants
+(name, address, priceRange, email, phone) VALUES 
+('The First Class', '1 Georgian Dr', 1, 'tfc@georgiancollege.ca', '7057281968');
+
