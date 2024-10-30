e3d94e197986021beaca35b20d018915cabdfbfc
ifotn <Rich.Freeman@GeorgianCollege.ca>
2024-10-30 09:49:11 -0400

    l8/2 build all relationships

--- week8.sql
+++ week8.sql
@@ -27,3 +27,43 @@ DELETE FROM restaurants WHERE restaurantId = 12;
 -- add FK constraint to cuisineId
 ALTER TABLE restaurants
 ADD FOREIGN KEY (cuisineId) REFERENCES cuisines(cuisineId);
+
+SELECT * FROM products;
+
+ALTER TABLE products
+ADD COLUMN restaurantId INT NOT NULL;
+
+UPDATE products SET restaurantId = 1;
+
+ALTER TABLE products
+ADD FOREIGN KEY (restaurantId) REFERENCES restaurants(restaurantId);
+
+ALTER TABLE orders
+ADD COLUMN customerId INT NOT NULL,
+ADD COLUMN driverId INT;
+
+UPDATE orders SET customerId = 3;
+
+ALTER TABLE orders
+ADD FOREIGN KEY (customerId) REFERENCES customers(customerId);
+
+ALTER TABLE orders
+ADD COLUMN restaurantId INT NOT NULL;
+
+UPDATE orders SET restaurantId = 2;
+
+ALTER TABLE orders
+ADD FOREIGN KEY (restaurantId) REFERENCES restaurants(restaurantId);
+
+ALTER TABLE orders
+DROP COLUMN restaurant;
+
+-- create junction between orders & products since they have many to many relationship
+CREATE TABLE orderItems
+(orderItemId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+productId INT NOT NULL,
+orderId INT NOT NULL,
+quantity INT NOT NULL,
+price DECIMAL(6,2) NOT NULL,
+FOREIGN KEY (productId) REFERENCES products(productId),
+FOREIGN KEY (orderId) REFERENCES orders(orderId));
